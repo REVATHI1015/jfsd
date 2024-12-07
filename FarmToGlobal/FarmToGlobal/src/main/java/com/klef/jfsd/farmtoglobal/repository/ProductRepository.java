42d2164d4f3025f161c28da9ce285c84325d1755
REVATHI1015 <140580427+REVATHI1015@users.noreply.github.com>
2024-12-08 00:18:00 +0530

    Delete FarmToGlobal/FarmToGlobal directory

--- FarmToGlobal/FarmToGlobal/src/main/java/com/klef/jfsd/farmtoglobal/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.klef.jfsd.farmtoglobal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.klef.jfsd.farmtoglobal.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
